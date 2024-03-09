53389b1925056a08cd27f2d845c997dba172881a
OR <OR@DESKTOP-VMP5D68>
2024-03-09 10:16:52 +0530

    this my first project

---
+++ DemoAutomationFramework/src/test/java/pratice/SampleTest.java
@@ -0,0 +1,12 @@
+package pratice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void toLearn() {
+		Reporter.log("Hi",true);
+	}
+}
